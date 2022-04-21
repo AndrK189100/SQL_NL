a85917fec907c725f62ef973ac9b2569193af0b7
AndrK189100 <andrka@gmail.com>
2022-04-21 13:09:01 +0300

    refactoring

---
+++ data.py
@@ -0,0 +1,7 @@
+
+playlists_ids = ['06iN03o811VWPvLwayMETN', '1iqt8e4JKpN3YxIxy86djO', '4ZiYJzYXl6clxsL0er7fdu',
+                     '37i9dQZEVXbNG2KDcFcKOF', '37i9dQZEVXbLiRSasKsNU9', '37i9dQZEVXbNv6cjoMVCyg',
+                     '37i9dQZF1DXdxcBWuJkbcy', '37i9dQZF1DWVciwe52Zt0R']
+
+album_ids = ['4u5Ik7NMYl3EITJngbMS4V', '1YuRC8Fj5XdgpuoA7yBDnr', '5htuLCzUNQ5BRlngbw20Mu', '6SbrIpVsaJ5wgCQtMMwVR2',
+                 '16iIPsnAjGZea8TeOCzeN8']
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import psycopg2
 import json
 import sql_requests
 import sql_requests_2
-
+from data import playlists_ids, album_ids
 
 def create_json(data: list, file_name: str):
     with open(file_name, 'w', encoding='utf-8') as f:
@@ -167,15 +167,10 @@ if __name__ == '__main__':
     client_secret = ''
     db_pass = ''
 
-    playlists_ids = ['06iN03o811VWPvLwayMETN', '1iqt8e4JKpN3YxIxy86djO', '4ZiYJzYXl6clxsL0er7fdu',
-                     '37i9dQZEVXbNG2KDcFcKOF', '37i9dQZEVXbLiRSasKsNU9', '37i9dQZEVXbNv6cjoMVCyg',
-                     '37i9dQZF1DXdxcBWuJkbcy', '37i9dQZF1DWVciwe52Zt0R']
 
-    album_ids = ['4u5Ik7NMYl3EITJngbMS4V', '1YuRC8Fj5XdgpuoA7yBDnr', '5htuLCzUNQ5BRlngbw20Mu', '6SbrIpVsaJ5wgCQtMMwVR2',
-                 '16iIPsnAjGZea8TeOCzeN8']
 
-    #Раскомментировать.
-
+    # Раскомментировать.
+    #
     # get_playlists(playlists_ids, client_id=client_id, client_secret=client_secret, limit=20,
     #                 file_name = 'db_playlists.json')
     #
